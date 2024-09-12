74e74926cc243c3560a7745256be3313792b71cd
Joseph Burghard <josephburgharddev@gmail.com>
2024-09-12 08:50:38 -0400

    add gtm scripts

--- app/root.tsx
+++ app/root.tsx
@@ -27,19 +27,26 @@ export default function App() {
           <script
             async
             dangerouslySetInnerHTML={{
+              //   __html: `{
+              //   (function (w, d, s, l, i) {
+              //       w[l] = w[l] || [];
+              //       w[l].push({ "gtm.start": new Date().getTime(), "event": "gtm.js" });
+              //       var f = d.getElementsByTagName(s)[0],
+              //           j = d.createElement(s),
+              //           dl = l != "dataLayer" ? "&l=" + l : "";
+              //       j.async = true;
+              //       j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              //       f.parentNode.insertBefore(j, f);
+              //   })(window, document, "script", "dataLayer", "GTM-XXX");
+              //     ;
+              // }`,
               __html: `{
-              (function (w, d, s, l, i) {
-                  w[l] = w[l] || [];
-                  w[l].push({ "gtm.start": new Date().getTime(), "event": "gtm.js" });
-                  var f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : "";
-                  j.async = true;
-                  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                  f.parentNode.insertBefore(j, f);
-              })(window, document, "script", "dataLayer", "GTM-XXX");
-                ;
-            }`,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NCZFPV7S');
+}`,
             }}
           />
         </Suspense>
@@ -49,6 +56,16 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <noscript>
+          <iframe
+            title="noscript"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NCZFPV7S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Provider store={store}>
           <Header />
           <Outlet />
